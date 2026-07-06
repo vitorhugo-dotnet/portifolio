087383ce93edcdf901cd6e83f258af167f4dc604
Vitor Hugo <vitorhugoalvesferreira@gmail.com>
2026-07-06 14:44:27 -0300

    docs: register The Universe Decides Play Store routes

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,11 @@ import Index from "./pages/Index.tsx";
 import NotFound from "./pages/NotFound.tsx";
 import Hub from "./pages/Hub.tsx";
 import PrivacyPolicy from "./pages/PrivacyPolicy.tsx";
+import TheUniverseDecidesAppAccess from "./pages/TheUniverseDecidesAppAccess.tsx";
+import TheUniverseDecidesDataSafety from "./pages/TheUniverseDecidesDataSafety.tsx";
+import TheUniverseDecidesPrivacyPolicy from "./pages/TheUniverseDecidesPrivacyPolicy.tsx";
+import TheUniverseDecidesStoreListing from "./pages/TheUniverseDecidesStoreListing.tsx";
+import TheUniverseDecidesSupport from "./pages/TheUniverseDecidesSupport.tsx";
 
 const queryClient = new QueryClient();
 
@@ -20,6 +25,26 @@ const App = () => (
           <Route path="/" element={<Index />} />
           <Route path="/hub" element={<Hub />} />
           <Route path="/privacy-policy" element={<PrivacyPolicy />} />
+          <Route
+            path="/the-universe-decides/privacy-policy"
+            element={<TheUniverseDecidesPrivacyPolicy />}
+          />
+          <Route
+            path="/the-universe-decides/data-safety"
+            element={<TheUniverseDecidesDataSafety />}
+          />
+          <Route
+            path="/the-universe-decides/store-listing"
+            element={<TheUniverseDecidesStoreListing />}
+          />
+          <Route
+            path="/the-universe-decides/app-access"
+            element={<TheUniverseDecidesAppAccess />}
+          />
+          <Route
+            path="/the-universe-decides/support"
+            element={<TheUniverseDecidesSupport />}
+          />
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
           <Route path="*" element={<NotFound />} />
         </Routes>
